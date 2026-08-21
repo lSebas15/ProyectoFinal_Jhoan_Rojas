9f853e193dfec54432dccfedc95992f198d79609
Jhoan Rojas Medina <sebastianrojas2851@gmail.com>
2026-08-21 01:25:54 -0500

    Ejercicio realizado del proyecto final

--- src/main/java/www/cibertec/Main.java
+++ src/main/java/www/cibertec/Main.java
@@ -1,17 +1,56 @@
 package www.cibertec;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    public static double calcularCostoEntrada(int zona) {
+        double precio;
+
+        if (zona == 0) {
+            precio = 40.0;
+        } else if (zona == 1) {
+            precio = 120.0;
+        } else {
+            precio = 800.0;
         }
+
+            return precio;
     }
+
+    static void main() {
+
+            String fiesta = "pocoyo fest" , ubicacion = "";
+            int cantidadEntradas = 1, zona = 3;
+            double precioTrago = 196.50, costoEntrada, totalEntradas = 0;
+
+            switch (zona){
+                case 1:
+                    ubicacion = "General";
+                    break;
+                case 2:
+                    ubicacion = "VIP";
+                    break;
+                case 3:
+                    ubicacion = "Box";
+                    break;
+            }
+
+            costoEntrada = calcularCostoEntrada(zona);
+
+            for (int i = 1; i <= cantidadEntradas; i++) {
+                totalEntradas = totalEntradas + costoEntrada;
+                System.out.println("Numero de entrada: " + i + " - Costo: S/." + costoEntrada);
+            }
+
+            double totalPagar = totalEntradas + precioTrago;
+
+             // Salida de resultados
+                   System.out.println("Fiesta: " + fiesta);
+                   System.out.println("Ubicacion: " + ubicacion);
+                   System.out.println("Entradas Compradas: S/." + cantidadEntradas);
+                   System.out.println("Subtotal Entradas: S/." + totalEntradas);
+                   System.out.println("Precio del Trago: S/." + precioTrago);
+                   System.out.println("Total a Pagar: S/." + totalPagar);
+    }
+
 }
+
